ec4db7cf82fbd8d400c2e0f144f175e20dfbee01
Bartłomiej Martenka <94762027+BarthazZ@users.noreply.github.com>
2022-02-02 20:14:23 +0100

    Dodaj pliki projektów.

---
+++ Exercise 7 Mathematical operations square root/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_7_Mathematical_operations_square_root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A: Ask the user for a number.
+            Console.WriteLine("Please enter any number.");
+
+            // B: Remember the number provided by the user.
+            int an1 = int.Parse(Console.ReadLine());
+
+            // C: Calculate the square root with the Math.sqrt () method.
+            double an2 = Math.Sqrt(an1);
+
+            // D: View the result.
+            Console.WriteLine("The square root of z" + an1 + " is: " + an2);
+        }
+    }
+}
